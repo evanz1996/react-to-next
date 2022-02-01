edc8ebf348a7c15bb3127f8ff013d01f07bd43b7
Vanessa Valenzuela <vanessavalenzuela08@gmail.com>
2022-02-01 13:07:42 +0800

    first commit
    first commit

---
+++ my-next-react/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BlogCard1 } from '../../components/BlogCard1';
+
+function about() {
+  return (
+    <div>
+      <BlogCard1></BlogCard1>
+    </div>
+  );
+}
+
+export default about;
---
+++ src/components/cards/BlogCard13.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+
+// reactstrap components
+import { Card, CardBody, CardTitle } from 'reactstrap';
+
+// Core Components
+
+function BlogCard13() {
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  });
+  return (
+    <>
+      <Card className="card-blog" data-header="pattern">
+        <a href="#pablo" onClick={(e) => e.preventDefault()}>
+          <img
+            alt="..."
+            className="img pattern rounded"
+            src={require('assets/img/ill/presentation_bg.png')}
+          ></img>
+        </a>
+        <CardBody>
+          <h6 className="card-category text-danger">
+            <i className="ni ni-badge"></i> Wearing
+          </h6>
+          <CardTitle tag="h5">
+            <a href="#pablo" onClick={(e) => e.preventDefault()}>
+              Choose the best products
+            </a>
+          </CardTitle>
+          <p className="card-description">
+            Yesterday, as Facebook launched its news reader app Paper,
+            design-focused startup FiftyThree called out Facebook...
+          </p>
+        </CardBody>
+      </Card>
+    </>
+  );
+}
+
+export default BlogCard13;
